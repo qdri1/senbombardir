48faa1d31aa35b0a1fd238bd4f8b5914cec17ea1
k.alimtayev <k.alimtayev@tele2.kz>
2025-07-13 12:12:40 +0500

    version 1.9 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.alimapps.senbombardir"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
