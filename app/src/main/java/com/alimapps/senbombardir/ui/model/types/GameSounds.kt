64b5bb4b7755706c99d65d86ccbb30a49c5d1577
k.alimtayev <k.alimtayev@tele2.kz>
2025-10-23 12:38:34 +0500

    new sounds

--- app/src/main/java/com/alimapps/senbombardir/ui/model/types/GameSounds.kt
+++ app/src/main/java/com/alimapps/senbombardir/ui/model/types/GameSounds.kt
@@ -4,13 +4,17 @@ import com.alimapps.senbombardir.R
 
 enum class GameSounds(val stringRes: Int, val rawRes: Int) {
     Whistle(stringRes = R.string.sound_whistle, rawRes = R.raw.whistle),
+    LeagueChemp(stringRes = R.string.sound_league_chemp, rawRes = R.raw.sound_league_chemp),
+    GoalFans(stringRes = R.string.sound_goal_fans, rawRes = R.raw.sound_goal_fans),
+//    Anarbekov(stringRes = R.string.sound_anarbekov, rawRes = R.raw.sound_anarbekov),
+    UoUoUo(stringRes = R.string.sound_uo_uo_uo, rawRes = R.raw.sound_uo_uo_uo),
     StadiumApplause(stringRes = R.string.sound_stadium_applause, rawRes = R.raw.stadium_applause),
     GirlsApplause(stringRes = R.string.sound_girls_applause, rawRes = R.raw.girls_applause),
     BilgeninIstepJatyr(stringRes = R.string.sound_bilgenin_istep_jatyr, rawRes = R.raw.bilgenin_istep_jatyr),
     GoalSave(stringRes = R.string.sound_goal_save, rawRes = R.raw.goal_save),
+    ModrichtynPasy(stringRes = R.string.sound_modrichtyn_pasy, rawRes = R.raw.modrichtyn_pasy),
+    TondyrypTastagan(stringRes = R.string.sound_tondyryp_tastagan, rawRes = R.raw.tondyryp_tastagan),
     Suiii(stringRes = R.string.sound_suiii, rawRes = R.raw.suiiiii),
     SuiiiFull(stringRes = R.string.sound_suiii_full, rawRes = R.raw.suiii_full),
     GoalGoalGoal(stringRes = R.string.sound_goal_goal_goal, rawRes = R.raw.gol_gol_gol),
-    ModrichtynPasy(stringRes = R.string.sound_modrichtyn_pasy, rawRes = R.raw.modrichtyn_pasy),
-    TondyrypTastagan(stringRes = R.string.sound_tondyryp_tastagan, rawRes = R.raw.tondyryp_tastagan),
 }
