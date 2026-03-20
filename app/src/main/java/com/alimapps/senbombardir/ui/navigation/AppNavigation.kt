aa55baf955308eb047ec5bad507f70369b0b4ca7
k.alimtayev <k.alimtayev@tele2.kz>
2026-03-20 15:19:31 +0500

    bottom nav colors

--- app/src/main/java/com/alimapps/senbombardir/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/alimapps/senbombardir/ui/navigation/AppNavigation.kt
@@ -13,8 +13,10 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -74,6 +76,13 @@ fun AppNavigation(
                 ) {
                     bottomNavItems.forEach { item ->
                         NavigationBarItem(
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedIconColor = MaterialTheme.colorScheme.primary,
+                                selectedTextColor = MaterialTheme.colorScheme.primary,
+                                indicatorColor = Color.Transparent,
+                                unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                            ),
                             selected = currentRoute == item.route,
                             onClick = {
                                 navController.navigate(item.route) {
