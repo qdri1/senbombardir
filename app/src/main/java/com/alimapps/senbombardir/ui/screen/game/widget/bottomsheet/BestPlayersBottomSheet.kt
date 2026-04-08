2d5e57a9fb498394b8317e859877410758119886
k.alimtayev <k.alimtayev@tele2.kz>
2026-04-08 16:53:44 +0500

    self review

--- app/src/main/java/com/alimapps/senbombardir/ui/screen/game/widget/bottomsheet/BestPlayersBottomSheet.kt
+++ app/src/main/java/com/alimapps/senbombardir/ui/screen/game/widget/bottomsheet/BestPlayersBottomSheet.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.alimapps.senbombardir.R
 import com.alimapps.senbombardir.ui.model.BestPlayerUiModel
