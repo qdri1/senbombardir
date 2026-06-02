bef1fc16477900fc5a9f35d0942dbd9293031206
k.alimtayev <k.alimtayev@tele2.kz>
2026-06-02 17:15:19 +0500

    6.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alimapps.senbombardir"
         minSdk = 24
         targetSdk = 36
-        versionCode = 68
-        versionName = "6.8"
+        versionCode = 69
+        versionName = "6.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
