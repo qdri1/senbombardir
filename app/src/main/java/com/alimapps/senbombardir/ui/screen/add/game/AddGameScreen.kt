4cce3e06f87a3a3ab29f2e746c5ec8aeb77dd22f
k.alimtayev <k.alimtayev@tele2.kz>
2026-06-11 11:52:46 +0500

    toolbar shadow add game screen

--- app/src/main/java/com/alimapps/senbombardir/ui/screen/add/game/AddGameScreen.kt
+++ app/src/main/java/com/alimapps/senbombardir/ui/screen/add/game/AddGameScreen.kt
@@ -54,6 +54,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -133,8 +134,10 @@ private fun AddGameScreenContent(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .shadow(elevation = 4.dp)
                     .background(MaterialTheme.colorScheme.surface)
-                    .padding(16.dp),
+                    .padding(vertical = 8.dp)
+                    .padding(horizontal = 16.dp),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 Icon(
